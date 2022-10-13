e31ef803127d7697d5ef2e8f6f85a0c07dd20c73
Esteban Munoz <esteban.munozal@gmail.com>
2022-10-13 00:06:06 -0500

    Add horizontal scrollview

--- layoutsnative/App.js
+++ layoutsnative/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Image} from 'react-native';
+import {StyleSheet, View, Image, Text, ScrollView} from 'react-native';
 
 const App = () => {
   return (
@@ -11,6 +11,41 @@ const App = () => {
             source={require('./assets/img/bg.jpg')}
           />
         </View>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Que hacer en Paris</Text>
+          <ScrollView horizontal>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad1.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad3.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad4.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad5.jpg')}
+              />
+            </View>
+          </ScrollView>
+        </View>
       </View>
     </>
   );
@@ -20,6 +55,19 @@ const styles = StyleSheet.create({
   banner: {
     height: 250,
     flex: 1,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
   },
 });
 
